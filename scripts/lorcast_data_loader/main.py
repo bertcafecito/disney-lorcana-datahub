0fa298734c1d29db757ca2905a3b5366839ec0a5
Bert Cafecito <189543060+bertcafecito@users.noreply.github.com>
2025-05-11 12:51:26 -0400

    Create main.py

---
+++ scripts/lorcast_data_loader/main.py
@@ -0,0 +1,21 @@
+import os
+import sqlite3
+
+class LorcanaDataLoader:
+    def __init__(self, db_name="data/processed/lorcana.db"):
+        self.db_name = db_name
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+
+  def database_exists(self):
+          """Check if the SQLite database exists."""
+          return os.path.exists(self.db_name)
+
+def main():
+  loader = LorcanaDataLoader()
+
+  # Check if the database exists
+  if loader.database_exists():
+    print(f"Database '{loader.db_name}' exists.")
+  else:
+    print(f"Database '{loader.db_name}' does not exist.")
